365237f48b97fde3954f77556ad8c506700717a4
trucnguyen13 <48061497+trucnguyen13@users.noreply.github.com>
2019-06-07 01:21:56 +0700

    Update main.cpp

--- Digital-Image-Processing/main.cpp
+++ Digital-Image-Processing/main.cpp
@@ -1,70 +1,77 @@
-#include <stdio.h>
-#include <iostream>
-#include "Bitmap.h"
+#include "Effect.h"
+#include "Transform.h"
 using namespace std;
 //Test
 int main()
 {
-	char *inFileName = (char*)malloc(100*sizeof(char));
-	const char *outFileName= "out.bmp";
-	Bitmap bmp, outbmp;
+	char *inFileName = (char*)malloc(100 * sizeof(char));
+	const char *outFileName = "out.bmp";
+	Bitmap bmp;
 	printf("Enter file path:");
 	scanf("%s", inFileName);
 	int option;
 	char next = 'Y';
-	if(LoadBitmap(inFileName, bmp))
+	if (LoadBitmap(inFileName, bmp))
 	{
 		printf("Image Size: %dx%d\n", bmp.width, bmp.height);
 		while (next == 'Y' || next == 'y')
 		{
-			printf("Choose an option:\n1.Black White\n2.AdjustBrightess\n3.reverse.\n4.Zoomimage\n");
+			cout << "Choose an option:\n"
+				<< "\n1. Cut Image"
+				<< "\n2. Balance Histogram"
+				<< "\n3. Bunch Image"
+				<< "\n4. Picture Frames"
+				<< "\n5. Create Drawing"
+				<< endl;
 			scanf("%d", &option);
 			switch (option)
 			{
 			case 1:
-				
-				BlackWhite(bmp);
+				int x, y, X, Y;
+				printf("Nhập tọa độ đầu (x,y): \n");
+				scanf("%d%d", &x, &y);
+				printf("Nhập tọa độ sau (X,Y): \n");
+				scanf("%d%d", &X, &Y);
+				CutImage(bmp, x, y, X, Y);
 				break;
 			case 2:
-				AdjustBrightness(bmp, 2);
+				BalanceHistogram(bmp);
 				break;
 			case 3:
-				int k;
-				printf("1.Dao nguoc theo be doc.\n2.dao nguoc theo be ngang\n");
-				scanf("%d", &k);
-				reverse_image(bmp,k);
+				int new_level;
+				printf("Nhập độ độ bó cụm của ảnh: \n");
+				scanf("%d", &new_level);
+				BunchImage(bmp, new_level);
 				break;
 			case 4:
-				int m;
-				printf("Nhap do zoom: ");
-				scanf("%d", &m);
-				zoomimage(bmp, outbmp, m);
-				break;
+				int thickness, COLOR;
+				printf("Nhập độ dày khung: \n");
+				scanf("%d", &thickness);
+				printf("Nhập màu sắc khung: \n");
+				scanf("%d", &COLOR);
+				PictureFrames(bmp, thickness, COLOR);
+			case 5:
+				int boundary;
+				printf("Nhập độ nét của tranh vẽ: \n");
+				scanf("%d", &boundary);
+				FindBoundary(bmp, boundary);
 			default:
 				printf("Wrong command!");
 				break;
 			}
 			if (!SaveBitmap(outFileName, bmp))
-				printf("Can not save the bitmap file!!!\n");
-			#ifdef _WIN32
-				system("start out.bmp");
-			#else
-				system("open out.bmp");
-			#endif
-			printf("Would you like to continue?(Y/N)");
-			scanf("\n%c", &next);
-			#ifdef _WIN32
-				system("cls");
-			#else
-				system("clear");
-			#endif
+				cout << "Can not save the bitmap file!!!\n";
+			system("start out.bmp");
+			system("cls");
+			cout << "Would you like to continue?(Y/N)\n";
+			cin >> next;
 		}
 		DisposeBitmap(bmp);
 	}
 	else
 		printf("Can not load the bitmap file!!!\n");
-	
+
 	printf("Bye!\n");
-	
+
 	return 0;
 }
